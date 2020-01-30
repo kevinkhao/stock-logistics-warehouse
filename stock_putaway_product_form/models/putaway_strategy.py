be79d2acd6286d265d0f88002e2f364322689a78
Kevin Khao <kevinkhao@gmail.com>
2020-01-30 17:22:13 +0100

    [ADD] 12.0: stock_putaway_product_form

---
+++ stock_putaway_product_form/models/putaway_strategy.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class FixedPutAwayStrategy(models.Model):
+    _inherit = "stock.fixed.putaway.strat"
+
+    product_tmpl_id = fields.Many2one(
+        comodel_name="product.template",
+        related="product_id.product_tmpl_id",
+        readonly=True,
+        store=True,
+    )
---
+++ stock_putaway_product_form/readme/USAGE.rst
@@ -0,0 +1,1 @@
+* Open the form view of a product template or a product and use the fields located under the "Inventory" tab
